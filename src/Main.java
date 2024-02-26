95b72a7070c6623a4348d35a9021f5f4106ca953
Nataliia Medvedeva <shishika87@gmail.com>
2024-02-26 20:14:52 +0700

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        int ticketPrice = 14256;
+
+        int bonus = ticketPrice / 20;
+        System.out.println("Количество начисленных миль за купленный билет: " + bonus);
+    }
+}
